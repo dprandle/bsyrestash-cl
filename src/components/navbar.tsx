53c3f845d37d6f2f61d2a06b8b8cfd8fefebda2d
dprandle <daniel@zetrickllc.com>
2025-04-13 21:55:53 -0500

    Work on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,24 @@
 import styles from './navbar.module.css';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_left}>
         <ul>
-          <li className={styles.navbar_item}><Link to="/">Home</Link></li>
+          <li><NavLink to="/" className={styles.navbar_item}>Home</NavLink></li>
         </ul>
       </div>
       <div className={styles.navbar_center}>
         <ul>
-          <li className={styles.navbar_item}><a href="/products">Products</a></li>
-          <li className={styles.navbar_item}><a href="/about">About Us</a></li>
-          <li className={styles.navbar_item}><a href="/contact">Contact</a></li>
+          <li><NavLink to="/products" className={styles.navbar_item}>Products</NavLink></li>
+          <li><NavLink to="/about" className={styles.navbar_item}>About</NavLink></li>
+          <li><NavLink to="/contact" className={styles.navbar_item}>Contact</NavLink></li>
         </ul>
       </div>
       <div className={styles.navbar_right}>
         <ul>
-          <li className={styles.navbar_sign_in}><a href="/login">Sign in</a></li>
+          <li><NavLink to="/login" className={styles.navbar_sign_in}>Sign in</NavLink></li>
         </ul>
       </div>
     </nav>
